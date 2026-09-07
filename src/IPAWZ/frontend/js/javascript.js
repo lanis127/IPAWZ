f4a573e76b3a9e7203043141278c4d8068b2ecd3
Nathalia <89409034+nathaliafebroni@users.noreply.github.com>
2026-09-07 01:39:46 -0300

    Add files via upload

---
+++ src/IPAWZ/frontend/js/javascript.js
@@ -0,0 +1,27 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+
+if(openMenu){
+
+    openMenu.addEventListener("click", () => {
+
+        sidebar.classList.add("active");
+        overlay.classList.add("active");
+
+    });
+
+}
+
+closeMenu.addEventListener("click", closeSidebar);
+
+overlay.addEventListener("click", closeSidebar);
+
+function closeSidebar(){
+
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+
+}
